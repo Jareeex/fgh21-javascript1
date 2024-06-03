6c901a6c0266b2995802359cd4f25595a8960992
Jareeex <baswarafajar@gmail.com>
2024-06-03 15:14:12 +0700

    Update taks-03.js

--- taks-03.js
+++ taks-03.js
@@ -1,8 +1,8 @@
 const printSegitiga = 5; 
 function cetakSegitiga(num) {
-        for (var i = num; i >= 1; i--) {
-        var row = '';       
-        for (var j = 1; j <= i; j++) {
+        for (let i = num; i >= 1; i--) {
+        let row = '';       
+        for (let j = 1; j <= i; j++) {
             row += j + ' ';
         }
         console.log(row);
